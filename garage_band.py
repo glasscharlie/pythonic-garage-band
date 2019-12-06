c8b970f256380a920aaca004295c1691c44d7a82
glasscharlie <charlie-glass@live.com>
2019-12-06 08:36:04 -0800

    started lab

---
+++ garage_band.py
@@ -0,0 +1,20 @@
+class Band:
+    bands = []
+    def __init__(self, name, members):
+        self.name = name
+        self.__class__.members.append(self)
+        self.members = members
+
+     def play_solos(self):
+        for i in members:'
+            return '{members[i]}'
+
+    def __str__(self):
+        return f'Hello. We are mouserat, welcome to our show'
+
+class Musician:
+    
+
+
+
+
